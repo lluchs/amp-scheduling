63edc5562fae2d1da2b09f89208e370c05c3b8c5
Lukas Werling <lukas.werling@gmail.com>
2017-09-27 11:48:09 +0200

    test/micro: Add pointer_chasing memory access benchmark

--- test/micro.c
+++ test/micro.c
@@ -8,14 +8,23 @@
 #include "ultmigration.h"
 #include "random.h"
 
+// Overall iterations.
 #define WORK 10000
-#define BUFSIZE (size_t) (500 << 20)
+// Number of CPU work iterations.
+#define CPU_ITERATIONS 400000
+
+// Size of memory buffer used for indirect_access and pointer_chase
+#define BUFSIZE (size_t) (100 << 20)
+
+// indirect_access: Length of access pattern.
 #define PATTERN_LENGTH 100000
+// pointer_chasing: Number of steps per iteration.
+#define POINTER_CHASE_STEPS 7000
 
 static int lotsofwork() {
 	unsigned int foo1 = 2, foo2 = 3, foo3 = 4, foo4 = 5;
 	float bar1 = 2, bar2 = 3, bar3 = 4, bar4 = 5;
-	for (int i = 0; i < 100000; i++) {
+	for (int i = 0; i < CPU_ITERATIONS; i++) {
 		foo1 = foo1 * foo1; foo2 = foo2 + foo2; foo3 = foo3 * foo3; foo4 = foo4 + foo4;
 		bar1 = bar1 * bar1; bar2 = bar2 + bar2; bar3 = bar3 * bar3; bar4 = bar4 + bar4;
 		foo1 = foo1 * foo1; foo2 = foo2 + foo2; foo3 = foo3 * foo3; foo4 = foo4 + foo4;
@@ -24,27 +33,62 @@ static int lotsofwork() {
 	return (foo1 + foo2 + foo3 + foo4 + (unsigned) (bar1 + bar2 + bar3 + bar4)) % 3;
 }
 
-static char *buf;
+static char *indirect_access_buf;
 static size_t *access_pattern;
 
-static int littlework() {
+static int indirect_access() {
 	for (int i = 0; i < PATTERN_LENGTH; i++) {
-		buf[access_pattern[i]] = i;
+		indirect_access_buf[access_pattern[i]] = i;
 	}
-	return buf[access_pattern[0]];
+	return indirect_access_buf[access_pattern[0]];
 }
 
-static void init_littlework() {
-	random_init();
-	buf = malloc(BUFSIZE);
+static void init_indirect_access() {
+	indirect_access_buf = malloc(BUFSIZE);
 	access_pattern = malloc(PATTERN_LENGTH * sizeof(*access_pattern));
 	for (int i = 0; i < PATTERN_LENGTH; i++) {
 		access_pattern[i] = random_next() % BUFSIZE;
 	}
 }
 
+#define CH_BUFLEN (BUFSIZE / sizeof(void*))
+static void **pointer_chasing_buf, **pointer_chasing_ptr;
+
+static int pointer_chasing() {
+	int r = 0;
+	for (int i = 0; i < POINTER_CHASE_STEPS; i++) {
+		pointer_chasing_ptr = *pointer_chasing_ptr;
+		r += (intptr_t) pointer_chasing_ptr;
+	}
+	return r;
+}
+
+static void init_pointer_chasing() {
+	pointer_chasing_buf = malloc(BUFSIZE);
+	// Initialize sequential list of pointers.
+	void **ptr;
+	for (ptr = pointer_chasing_buf; ptr < pointer_chasing_buf + CH_BUFLEN; ptr++) {
+		*ptr = ptr+1;
+	}
+	*(ptr-1) = pointer_chasing_buf;
+	// Shuffle the pointers.
+	size_t m = CH_BUFLEN, i;
+	void *tmp;
+	while (m) {
+		i = random_next() % m--;
+		tmp = pointer_chasing_buf[m];
+		pointer_chasing_buf[m] = pointer_chasing_buf[i];
+		pointer_chasing_buf[i] = tmp;
+	}
+	pointer_chasing_ptr = pointer_chasing_buf;
+}
+
+typedef int (*Fn)();
+
 int main() {
-	init_littlework();
+	random_init();
+	//init_indirect_access(); Fn littlework = indirect_access;
+	init_pointer_chasing(); Fn littlework = pointer_chasing;
 
 	ult_register_klt();
 	ult_migrate(ULT_FAST);
