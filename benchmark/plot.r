58f8755f2a238c93efc1b9e0ecb9eabcb2ec0514
Lukas Werling <lukas.werling@gmail.com>
2017-12-13 17:03:30 +0100

    benchmark: Add some graphs for swp results

--- benchmark/plot.r
+++ benchmark/plot.r
@@ -34,7 +34,9 @@ ggplot() +
 
 ggsave("powermeter.png", width = 100, height = 20, units = "cm", limitsize = FALSE)
 
+swp <- power_log %>% filter(str_detect(type, "swp"))
 non_swp <- power_log %>% filter(!str_detect(type, "swp"))
+
 # Graph to compare power behavior between fast/slow/fast+slow.
 ggplot(data = non_swp) +
 	geom_line(aes(x = duration, y = power, color = memory_bench)) +
@@ -43,3 +45,33 @@ ggplot(data = non_swp) +
 	facet_grid(cpu_ratio ~ memory_ratio, labeller = label_both)
 
 ggsave("fastslow-power.png", width = 20, height = 20, units = "cm")
+
+swp_fast <- swp %>% filter(str_detect(type, "fast"))
+swp_slow <- swp %>% filter(str_detect(type, "slow"))
+swp_combined <- inner_join(swp_fast, swp_slow, by = c("memory_bench", "cpu_ratio", "memory_ratio"), suffix = c(".fast", ".slow"))
+swp_cpi <- swp_combined %>%
+	gather(swp_cpu_cpi.fast, swp_mem_cpi.fast, key = "ft", value = "fast_cpi") %>%
+	gather(swp_cpu_cpi.slow, swp_mem_cpi.slow, key = "st", value = "slow_cpi") %>%
+	mutate(ft = substr(ft, 5, 7), st = substr(st, 5, 7)) %>%
+	filter(ft == st)
+g <- ggplot(data = swp_cpi) +
+	geom_point(aes(x = slow_cpi, y = fast_cpi, shape = ft, color = memory_bench), size = 2) +
+	geom_abline(intercept = 0, slope = 1) +
+	facet_grid(cpu_ratio ~ memory_ratio, labeller = label_both)
+
+ggsave("cpi.png", plot = g, width = 20, height = 20, units = "cm")
+ggsave("cpi_log.png", width = 20, height = 20, units = "cm",
+       plot = g + scale_x_log10() + scale_y_log10())
+
+ggplot(data = swp, mapping = aes(x = swp_cpu_l3, y = swp_mem_l3, color = memory_bench, shape = type)) +
+	geom_point() +
+	geom_line(aes(group = memory_bench)) +
+	facet_grid(cpu_ratio ~ memory_ratio, labeller = label_both)
+
+ggsave("l3.png", width = 20, height = 20, units = "cm")
+
+ggplot(data = swp) +
+	geom_point(aes(x = swp_cpu_l2, y = swp_mem_l2, color = memory_bench)) +
+	facet_grid(cpu_ratio ~ memory_ratio, labeller = label_both)
+
+ggsave("l2.png", width = 20, height = 20, units = "cm")
