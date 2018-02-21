eb80bf2833d4acbbc486c604ac8806c1a262be1a
Lukas Werling <lukas.werling@gmail.com>
2018-02-21 09:53:21 +0100

    test/ult_idle:  Change parameter to specify MWAIT core

--- test/ult_idle.c
+++ test/ult_idle.c
@@ -10,13 +10,19 @@
 
 int main(int argc, char **argv) {
 	ult_register_klt();
+	// The argument is the core to MWAIT on.
 	if (argc == 2) {
 		if (strcmp(argv[1], "fast") == 0)
-			ult_migrate(ULT_FAST);
-		else if (strcmp(argv[1], "slow") == 0)
 			ult_migrate(ULT_SLOW);
+		else if (strcmp(argv[1], "slow") == 0)
+			ult_migrate(ULT_FAST);
+		else {
+			printf("Usage: %s <core which MWAITs: fast/slow>\n", argv[0]);
+			return 1;
+		}
+		printf("MWAIT on %s CPU\n", argv[1]);
 	}
-	printf("Idling on CPU %d\n", sched_getcpu());
+	printf("Pausing on CPU %d\n", sched_getcpu());
 	pause();
 	ult_unregister_klt();
 }
